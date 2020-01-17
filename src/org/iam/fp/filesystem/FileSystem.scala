cb2fce72562b45aa382d69603fd054898f3589f4
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-01-17 12:04:34 +0000

    Making the bloody thing functional.  FFS!

--- src/org/iam/fp/filesystem/FileSystem.scala
+++ src/org/iam/fp/filesystem/FileSystem.scala
@@ -8,11 +8,17 @@ import org.iam.fp.files.Directory
 object FileSystem extends App {
 
   val root = Directory.ROOT
-  var state = State(root, root)
-  val scanner = new Scanner(System.in)
 
-  while (true) {
-    state.show
-    state = Command.from(scanner.nextLine())(state)
-  }
+  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+    currentState.show
+    Command.from(newLine).apply(currentState)
+  })
 }
+
+//  var state = State(root, root)
+//  val scanner = new Scanner(System.in)
+//
+//  while (true) {
+//    state.show
+//    state = Command.from(scanner.nextLine())(state)
+//  }
