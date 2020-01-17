cb679a7aecc59224fed67fcd670314268fcb0015
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-01-17 14:11:31 +0000

    making it entirely functional didn't work - revrting to using a var so that it will work

--- src/org/iam/fp/filesystem/FileSystem.scala
+++ src/org/iam/fp/filesystem/FileSystem.scala
@@ -9,16 +9,16 @@ object FileSystem extends App {
 
   val root = Directory.ROOT
 
-  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
-    currentState.show
-    Command.from(newLine).apply(currentState)
-  })
-}
+//  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+//    currentState.show
+//    Command.from(newLine).apply(currentState)
+//  })
 
-//  var state = State(root, root)
-//  val scanner = new Scanner(System.in)
-//
-//  while (true) {
-//    state.show
-//    state = Command.from(scanner.nextLine())(state)
-//  }
+  var state = State(root, root)
+  val scanner = new Scanner(System.in)
+
+  while (true) {
+    state.show
+    state = Command.from(scanner.nextLine())(state)
+  }
+}
